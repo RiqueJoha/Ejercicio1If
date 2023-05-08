8b6acc80c8629c17a2dee875b9e6750e8f1f3855
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-05-07 22:08:20 -0300

    Mejorado

--- if.js
+++ if.js
@@ -1,10 +1,17 @@
+const inputUsuario= document.querySelector("#usuario-nombre");
+const inputContrasenia= document.querySelector("#usuario-contrasenia");
+const pResultado= document.querySelector("#resultado");
+
+
+
+
 const USUARIO_NOMBRE_REGISTRADO = "Carlos";
 const USUARIO_CONTRASENIA_REGISTRADA = "Carlos1.";
 const USUARIO_NOMBRE_REGISTRADO2= "Juan";
 const USUARIO_CONTRASENIA_REGISTRADA2 = "Juanito";
 
-let usuarioNombreIngresado = prompt ("Ingrese Usuario")
-let usuarioContraseniaIngresada = prompt ("Ingresar Contraseña")
+/* let usuarioNombreIngresado = prompt ("Ingrese Usuario")
+let usuarioContraseniaIngresada = prompt ("Ingresar Contraseña") */
 
 /* if ((usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA) && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO)) {
     document.write ("Usuario y Contraseña Correctos")
@@ -12,8 +19,20 @@ let usuarioContraseniaIngresada = prompt ("Ingresar Contraseña")
     document.write ("Usuario y Contraseña Incorrectos")
 } */
 
-if (((usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA) && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) || (usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA2) && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO2))) {
-    document.write ("Usuario y Contraseña Correctos")
-} else {
-    document.write ("Usuario y Contraseña Incorrectos")
+
+let usuarioNombreIngresado= "def nombre";
+let usuarioContraseniaIngresada= "def contrasenia";
+
+function verificar(){
+
+    let usuarioNombreIngresado=inputUsuario.value;
+    let usuarioContraseniaIngresada=inputContrasenia.value;
+    if (((usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA) && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) || (usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA2) && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO2))) {
+        /* document.write ("Usuario y Contraseña Correctos") */
+        pResultado.innerHTML= "Usuario y Contraseña Correctos " + usuarioNombreIngresado;
+    } else {
+       /*  document.write ("Usuario y Contraseña Incorrectos") */
+       pResultado.innerHTML= "Usuario y Contraseña Incorrectos";
+       pResultado.style.color= "red";
+    }
 }
